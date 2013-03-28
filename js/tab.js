0754e13f631a04ff9f9167572a6fdc73be208c43
lightning <lightningtgc@gmail.com>
2013-03-28 12:44:41 +0800

    tab gesture
    Signed-off-by: lightning <lightningtgc@gmail.com>

--- js/tab.js
+++ js/tab.js
@@ -10,6 +10,10 @@ cnMobile.$package("MUI",function(cm){
 	}
 	var isTouchDevice = cm.platform.touchDevice;
 	var touchEvt = isTouchDevice ? "tap":"click";
+	/*
+	*tab参数:id,selctedClass,currentIndex,animate,animateDuration,gesture.
+	*
+	*/
 	Tab.prototype = {
 		init:function(options){
 			this.elem = $D.id(options.id);
@@ -26,7 +30,10 @@ cnMobile.$package("MUI",function(cm){
 			if(this.animate){
 				this.animateDuration = options.animateDuration || "1s";
 			}
-
+			//手势滑动tab页
+			this.gesture = options.gesture;
+			this.touchId = options.touchId;
+		 
 			this.bindHandlers();
 			this._setIndex();
 			this._initStyle();
@@ -34,7 +41,47 @@ cnMobile.$package("MUI",function(cm){
 			$D.addClass(this.tabs[this.currentIndex] ,this.selectedClass);
 		},
 		bindHandlers:function(){
+			var self = this;
 			$E.on(this.elem,touchEvt ,cm.bind(this._onClick,this));
+			if(this.gesture){
+					var touchId = $D.id(this.touchId); 
+					var isTouchDevice = cm.platform.touchDevice;
+					var startEvt = isTouchDevice ? "touchstart" : "mousedown";
+					var moveEvt = isTouchDevice ? "touchmove" : "mousemove";
+					var endEvt = isTouchDevice ? "touchend" : "mouseup";
+					var startClientX,
+						endClientX,
+					    currentIndex1,
+						currentIndex2;
+					//手势距离
+					var gestureLen = 100;
+				   // var currentIndex = self.currentIndex;  
+				   var tabsLen = self.tabs.length - 1;
+					$E.on(touchId, startEvt, function(e){
+						startClientX = isTouchDevice ? e.changedTouches[0].clientX : e.clientX;
+					});
+					$E.on(touchId, moveEvt, function(e){
+                          e.preventDefault();
+					});   
+					$E.on(touchId, endEvt, function(e){
+
+						endClientX = isTouchDevice ? e.changedTouches[0].clientX : e.clientX;  
+						var clientXWidth = startClientX - endClientX;
+						
+						if(clientXWidth > gestureLen){
+                             currentIndex1 = self.currentIndex -1 >= 0 ? self.currentIndex - 1 : tabsLen;			
+							self._setSelectedClass(currentIndex1); 
+							self.currentIndex = currentIndex1;
+							
+						}else if(clientXWidth < -gestureLen){
+							currentIndex2 = self.currentIndex + 1 > tabsLen ? 0 : self.currentIndex + 1 ; 
+							self._setSelectedClass(currentIndex2);
+							self.currentIndex = currentIndex2;
+						}
+					});
+
+				}
+
 		},
 		getIndex:function(ele){
 			return ele.getAttribute("_index");
@@ -50,7 +97,7 @@ cnMobile.$package("MUI",function(cm){
 			var tb_cons = this.tabContents;
 			var ori_w = this.tabBody.clientWidth;
 			var ori_item_w = tb_cons[0].clientWidth;
-	
+
 			if(this.animate){
 				$D.setStyle(this.tabBody,"-webkit-transition" , "all," + this.animateDuration);
 			}
@@ -61,7 +108,7 @@ cnMobile.$package("MUI",function(cm){
 			var tabContents = this.tabContents;
 			var currentIndex = this.currentIndex;
 			if(currentIndex == selectedIndex) return;
-			
+
 			$D.removeClass(tabs[currentIndex],this.selectedClass);
 			$D.addClass(tabs[selectedIndex],this.selectedClass);
 
@@ -79,7 +126,7 @@ cnMobile.$package("MUI",function(cm){
 			var target = e.target || e.srcElement;
 			if(isListItem(target) && target.parentNode == self.tabHeader){
 				var selectedIndex = self.getIndex(target);
-				
+
 				this._setSelectedClass(selectedIndex);
 				this.currentIndex = selectedIndex;
 
